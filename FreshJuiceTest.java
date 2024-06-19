6f035423ad4e9332ec6183a117cfb55ac26db1ab
aVoKaD-0 <139006972+aVoKaD-0@users.noreply.github.com>
2024-06-19 16:52:16 +0500

    перевод готов, осталось немного и готово

--- FreshJuiceTest.java
+++ FreshJuiceTest.java
@@ -155,7 +155,7 @@ public class FreshJuiceTest {
       return number2;
    }
 
-   static String  Check_e(String number) {
+   static String Check_e(String number) {
       String number2 = number;
       if (number.indexOf("e") != -1) {
          number2 = "0." + StringUtils.leftPad("", (Integer.parseInt(String.valueOf(number).substring(String.valueOf(number).indexOf("e")+2, String.valueOf(number).length()))-1), '0');
@@ -167,6 +167,314 @@ public class FreshJuiceTest {
       }
       return "number2";
    }
+
+   static String VynosIzPodKoren(String koren) { //вынос из под корня
+      try {
+         int c = 0; // проверка правильно ли стоит знак корня в строке
+         int f = 0; // проверка правильно ли стоит знак деления
+         int g = 0; // проверка правильно ли стоят числа с плавающей запятой
+         int f2 = 0; // проверка не стоит ли знак деления в конце или в начале
+         int g2 = 0; // проверка не стоит ли точка в конце или в начале
+         int c2 = 0; // проверка не стоит ли знак корня в конце
+         for (int i = 0; i < (koren).length()-1; i++) {
+            if (koren.substring(i) == "√") {
+               c += 1;
+            }
+            if (koren.substring(i) == "/") {
+               f += 1;
+            }
+            if (koren.substring(i) == ".") {
+               g += 1;
+            }
+            if (koren.substring(i) == "√" && i == (koren).length()) {
+               c2 += 1;
+            }
+            if (koren.substring(i) == "." && (i != 0 && i != (koren).length()-1) && koren.substring(i-1).isdigit() && koren.substring(i+1).isdigit()) {
+               g2 += 1;
+            }
+            if (koren.substring(i) == "/" && (i != 0 && i != (koren).length()-1) && koren.substring(i-1).isdigit() && koren.substring(i+1).isdigit()) {
+               f2 += 1;
+            }
+         }
+         if (g > 4 || c != 1 || f > 2 || f != f2 || g != g2 || c2 != 0) {
+               return "Проверьте правильность ввода";
+         } else if (koren.length() == 1) {
+               return "Проверьте правильность ввода";
+         } else {
+            int a = koren.indexOf("√");
+            boolean NeKPL = true; // проверяет есть ли число с плавающей запятой вне корня
+            boolean KPL = true; // проверяет есть ли число с плавающей запятой под корнем
+            boolean KDR = true; // проверяет есть ли дробь под корняем
+            boolean NeKDR = true; // проверяет есть ли дробь вне корня
+            int Drob_Ne = 0;
+            int Drob_V = 0;
+            Double numerator = 0.0;
+            for (int i = 0; i < koren.length(); i++) {
+               if (koren.substring(i) == ".") {
+                  KPL = false;
+               }
+            }
+            for (int i = 0; i < a; i++) {
+               if (koren.substring(i) ==".") {
+                  NeKPL = false;
+               }
+            }
+            for (int i = 0; i < koren.length(); i++) {
+               if (koren.substring(i) == "/") {
+                  KDR = false;
+                  Drob_V = i;
+               }
+            }
+            for (int i = 0; i < a; i++) {
+               if (koren.substring(i) == "/") {
+                  NeKDR = false;
+                  Drob_Ne = i;
+               }
+            }
+            if (KDR == true && (KPL == true || KPL == false)) {
+               if (koren.substring(a+1, koren.length()-1).indexOf(".") == -1) {
+                  number, number2 = Kur.Vynos_celogo_chisla(Integer.parseInt(koren.substring(a+1, koren.length()-1)));
+               } else {
+                  number, number2 = Kur.Vynos_NeCelogo_chisla(Double.parseDouble(koren.substring(a+1, koren.length()-1)));
+               }
+               if (NeKDR == true && (NeKPL == true || NeKPL == false)) { // если вне корня либо целое число, либо числа нету
+                  if (number == 0 && number2 == 0) {
+                        return f"Число иррациональное";
+                  } else {
+                     if (a != 0) {
+                        if (koren.substring(0, a).isdigit() == true) { // если число вне корня целое
+                           numerator = Kur.Proverka_dlin_chisla(Double.parseDouble(number*Integer.parseInt(koren.substring(0, a))));
+                        } else if (a != 0) { // если число вне корня не целое
+                           if (String.toString(number*Double.parseDouble(koren.substring(0, a))).indexOf(-1) == "0" && String.toString(number*Double.parseDouble(koren.substring(0, a))).indexOf(-2) == ".") {
+                                 numerator = str(number*Double.parseDouble(koren.substring(0, a))).substring(0, -2);
+                           } else {
+                                 numerator = Kur.Proverka_dlin_chisla(Double.parseDouble(number*Double.parseDouble(koren.substring(0, a))));
+                           }
+                        }
+                     } else {
+                        numerator = number;
+                     }
+                     numerator = Kur.Check_e(numerator);
+                     number2 = Kur.Check_e(number2);
+                     if (number2 == 0) { // если вне дроби есть число и число расскладывается нацело
+                        return f"{numerator}";
+                     } else if (number2 != 0) { // если вне дроби есть число и число расскладывается нацело
+                        return f"{numerator}√{number2}";
+                     } else {
+                        if (number2 == 0) {
+                           return f"{number}";
+                        } else {
+                           return f"{number}√{number2}";
+                        }
+                     }
+                  }
+               } else if (NeKDR == false && (NeKPL == true || NeKPL == false)) { // если вне корня дробь
+                  if (number == 0 && number2 == 0) {
+                        return f"Число иррациональное";
+                  } else {
+                        if (koren.substring(0, Drob_Ne).isdigit() == true) { // если число вне корня целое
+                           numerator = Kur.Proverka_dlin_chisla(Double.parseDouble(number*Integer.parseInt(koren.substring(0, Drob_Ne))));
+                        } else if (a != 0) { // если число вне корня не целое
+                           if (String.toString(number*Double.parseDouble(koren.substring(0, Drob_Ne))).indexOf(-1) == "0" && String.toString(number*Double.parseDouble(koren.substring(0, Drob_Ne))).indexOf(-2) == ".") {
+                              numerator = String.toString(number*Double.parseDouble(koren.substring(0, Drob_Ne))).substring(0, -2);
+                           } else {
+                              numerator = Kur.Proverka_dlin_chisla(Double.parseDouble(number*Double.parseDouble(koren.substring(0, Drob_Ne))));
+                           }
+                        }
+                        numerator = Kur.Check_e(numerator);
+                        number2 = Kur.Check_e(number2);
+                        if (koren.substring(0, a) != "" && number2 == 0) { // если вне дроби нету числа и число расскладывается нацело
+                           return f"{numerator}/{koren.substring(Drob_Ne+1, a)}";
+                        } else if (koren.substring(0, a) != "" && number2 != 0) { // если вне дроби есть число и число расскладывается нацело
+                           return f"{numerator}√{number2}/{koren.substring(Drob_Ne+1, a)}";
+                        }
+                  }
+               }
+            } else if (KDR == false && (KPL == false || KPL == true)) {
+               if (koren.substring(a+1, Drob_V).indexOf(".") == -1) {
+                  number, number2 = Kur.Vynos_celogo_chisla(Integer.parseInt(koren.substring(a+1, Drob_V)));
+               } else {
+                  number, number2 = Kur.Vynos_NeCelogo_chisla(Double.parseDouble(koren.substring(a+1, Drob_V)));
+               }
+               if (koren.substring(Drob_V+1, ).find(".") == -1) {
+                  number3, number4 = Kur.Vynos_celogo_chisla(Integer.parseInt(koren.substring(Drob_V+1, )));
+               } else {
+                  number3, number4 = Kur.Vynos_NeCelogo_chisla(Double.parseDouble(koren.substring(Drob_V+1, )));
+               }
+               if (NeKDR == true && (NeKPL == true || NeKPL == false)) { // если вне корня либо целое число, либо числа нету
+                  if ((number == 0 && number2 == 0) || (number3 == 0 && number4 == 0)) {
+                        return f"Число иррациональное";
+                  } else {
+                        if (a != 0) {
+                           if (String.toString(koren.substring(0, a)).isdigit() == true) { // если число вне корня целое
+                              numerator = Kur.Proverka_dlin_chisla(float(number*int(koren.substring(0, a))));
+                           } else if (a != 0) { // если число вне корня не целое
+                              if (String.toString(number*float(koren.substring(0, a))).indexOf(-1) == "0" && str(number*float(koren.substring(0, a))).indexOf(-2) == ".") {
+                                    numerator = str(number*float(koren.substring(0, a))).substring(0, -2);
+                              } else if (a != 0) {
+                                    numerator = Kur.Proverka_dlin_chisla(float(number*float(koren.substring(0, a))));
+                              }
+                           }
+                        } else {
+                           numerator = number;
+                        }
+                        numerator = Kur.Check_e(numerator);
+                        number2 = Kur.Check_e(number2);
+                        number3 = Kur.Check_e(number3);
+                        number4 = Kur.Check_e(number4);
+                        if (number2 == 0 && number4 == 0) { // если исло расскладывается нацело и знаменатель расскадывается нацело
+                           return f"{numerator}/{number3}";
+                        } else if (number2 == 0 && number4 != 0) { // если число расскладывается нацело, но знаменатель расскадывается не нацело
+                           return f"{numerator}/{number3}√1/{number4}";
+                        } else if (number2 != 0 && number4 == 0) { // если число расскладывается нацело, но знаменатель расскадывается нацело
+                           return f"{numerator}/{number3}√{number2}";
+                        } else if (number2 != 0 && number4 != 0) { // если число расскладывается нацело и знаменатель расскадывается не нацело
+                           return f"{numerator}√{number2}/{number3}√{number4}";
+                        }
+                  }
+               } else if (NeKDR == false && (NeKPL == false || NeKPL == true)) { // если вне корня либо дробь с целыми числами, либо дробь с числа с плавающей запятой, либо смешаная дробь
+                  if ((number == 0 && number2 == 0) || (number3 == 0 && number4 == 0)) {
+                        return f"Число иррациональное";
+                  } else {
+                        if (String.toString(koren.substring(0, Drob_Ne)).isdigit() == true) { // если число вне корня целое
+                           numerator = Kur.Proverka_dlin_chisla(Double.parseDouble(number*Integer.parseInt(koren.substring(0, Drob_Ne))));
+                        } else if a != 0 {// если число вне корня не целое
+                           if String.toString(number*Double.parseDouble(koren.substring(0, Drob_Ne))).indexOf(-1) == "0" && String.toString(number*Double.parseDouble(koren.substring(0, Drob_Ne))).indexOf(-2) == "." {
+                              numerator = String.toString(number*Double.parseDouble(koren.substring(0, Drob_Ne))).substring(0, -2);
+                           }
+                           else {
+                              numerator = Kur.Proverka_dlin_chisla(Double.parseDouble(number*Double.parseDouble(koren.substring(0, Drob_Ne))));
+                           }
+                        }
+                        if String.toString(koren.substring(Drob_Ne+1, a)).isdigit() == true { // если число в корне целое
+                           denominator = Kur.Proverka_dlin_chisla(float(number3*int(koren.substring(Drob_Ne+1, a))));
+                        } else if (a != 0) { // если число в корне не целое
+                           if str(number3*Double.parseDouble(koren.substring(Drob_Ne+1, a))).indexOf(-1) == "0" && String.toString(number3*Double.parseDouble(koren.substring(Drob_Ne+1, a))).indexOf(-2) == "." {
+                              denominator = String.toString(number3*Double.parseDouble(koren.substring(Drob_Ne+1, a))).substring(0, -2);
+                           } else {
+                              denominator = Kur.Proverka_dlin_chisla(Double.parseDouble(number3*Double.parseDouble(koren.substring(Drob_Ne+1, a))));
+                           }
+                        }
+                        numerator = Kur.Check_e(numerator);
+                        number2 = Kur.Check_e(number2);
+                        number3 = Kur.Check_e(number3);
+                        number4 = Kur.Check_e(number4);
+                        if (number2 == 0 && number4 == 0) { // если число расскладывается нацело и знаменатель расскадывается нацело
+                           return f"{numerator}/{denominator}";
+                        } else if (number2 == 0 && number4 != 0) { // если число расскладывается нацело, но знаменатель расскадывается не нацело
+                           return f"{numerator}/{denominator}√1/{number4}";
+                        }else if (number2 != 0 && number4 == 0) { // если число расскладывается нацело, но знаменатель расскадывается нацело
+                           return f"{numerator}/{denominator}√{number2}";
+                        } else if (number2 != 0 && number4 != 0) { // если число расскладывается нацело и знаменатель расскадывается не нацело
+                           return f"{numerator}√{number2}/{denominator}√{number4}";
+                        }
+                     }
+               }
+            }
+      } except {
+         return "Проверьте правильность ввода";
+      }
+   }
+
+   static String VnosPodKoren(String koren) {
+      try {
+         int c = 0;
+         int f = 0;
+         int g = 0;
+         int f2 = 0;
+         int g2 = 0;
+         int c2 = 0;
+         for (int i = 0; i < (koren).length()-1; i++) {
+            if (koren.substring(i) == "√") {
+               c += 1;
+            }
+            if (koren.substring(i) == "/") {
+               f += 1;
+            }
+            if (koren.substring(i) == ".") {
+               g += 1;
+            }
+            if (koren.substring(i) == "√" && i == 0) {
+               c2 += 1;
+            }
+            if (koren.substring(i) == "." && (i != 0 && i != (koren).length()-1) && Character.toString(koren.charAt(i-1)).isdigit() && Character.toString(koren.charAt(i+1)).isdigit()) {
+               g2 += 1;
+            }
+            if (koren.substring(i) == "/" && (i != 0 && i != (koren).length()-1) && Character.toString(koren.charAt(i-1)).isdigit() && Character.toString(koren.charAt(i+1)).isdigit()) {
+               f2 += 1;
+            }
+         }
+         if (Character.toString(koren.charAt(koren.length()-1)) == "√") {
+            c += 1;
+         }
+         if (g > 4 || c != 1 || f > 2 || f != f2 || g != g2 || c2 != 0) {
+            return "Проверьте правильность ввода";
+         } else if ((koren).length() == 1) {
+            return "Проверьте правильность ввода";
+         } else {
+            int a = String.toString(koren).indexOf("√");
+            int Drob_Ne = a;
+            int Drob_V = koren.length();
+            int number2 = 0;
+            for (int i = 0; i < (koren).length(); i++) {
+               if (koren.substring(i) == "/") {
+                     Drob_V = i;
+               }
+            }
+            for (int i = 0; i < a; i++) {
+               if (koren.substring(i) == "/") {
+                     Drob_Ne = i;
+               }
+            }
+            if (String.toString(koren.substring(0, Drob_Ne)).isdigit() == true) {
+               number = Integer.parseInt(koren.substring(0, Drob_Ne))**2;
+            } else{
+               number = Double.parseDouble(koren.substring(0, Drob_Ne))**2;
+            }
+            if (Drob_Ne != a) {
+               if (String.toString(koren.substring(Drob_Ne+1, a)).isdigit() == true) {
+                     number2 = Integer.parseInt(koren.substring(Drob_Ne+1, a))**2;
+               } else {
+                     number2 = Double.parseDouble(koren.substring(Drob_Ne+1, a))**2;
+               }
+            }
+            if (String.toString(koren.substring(a+1, Drob_V)).isdigit() == true && (koren.substring(a+1, Drob_V)).length() != 0) {
+               number *= Integer.parseInt(koren.substring(a+1, Drob_V));
+            } else if (str(koren.substring(a+1, Drob_V)).isdigit() != true && (koren.substring(a+1, Drob_V)).length() != 0) {
+               number *= Double.parseDouble(koren.substring(a+1, Drob_V));
+            }
+            if (Drob_V != (koren).length()) {
+               if (number2 == 0) {
+                  number2 = 1;
+               }
+               if (String.toString(koren.substring(Drob_V+1, koren.length()-1)).isdigit() == true && (koren.substring(Drob_V+1, koren.length()-1)).length() != 0) {
+                  number2 *= Integer.parseInt(koren.substring(Drob_V+1, ));
+               }
+               else if (String.toString(koren.substring(Drob_V+1, koren.length()-1)).isdigit() != true && (koren.substring(Drob_V+1, koren.length()-1)).length() != 0) {
+                  number2 *= Double.parseDouble(koren.substring(Drob_V+1, ));
+               }
+            }
+            if (type(number) == Double.parseDouble) {
+               if (String.toString(number).substring(-1) == "0" && str(number).substring(-2) == ".") {
+                  number = Integer.parseInt(str(number).substring(0, -2));
+               }
+            }
+            if (type(number2) == Double.parseDouble) {
+               if (str(number2).substring(-1) == "0" && str(number2).substring(-2) == ".") {
+                  number2 = Integer.parseInt(str(number2).substring(0, -2));
+               }
+            }
+            if (number2 == 0) {
+               return f"√{number}";
+            } else {
+               return f"√{number}/{number2}";
+            }
+         }
+      } except {
+            return "Проверьте правильность ввода";
+      }
+
+
    
    public static void main(String args[]){
       System.out.println(dom());
